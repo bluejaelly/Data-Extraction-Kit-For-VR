275b3053813a2954750e3eed3a2fc4340ccae3d7
Jaelly <38776787+bluejaelly@users.noreply.github.com>
2020-03-12 03:08:48 -0500

    VR Keyboard & Gaze Data Setup

--- Assets/DataKit/Scripts/GazeData.cs
+++ Assets/DataKit/Scripts/GazeData.cs
@@ -1,18 +1,54 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
+using VRTK;
 
 public class GazeData : MonoBehaviour
 {
+    [SerializeField] string csvName;
+    private bool startWrite;
     // Start is called before the first frame update
     void Start()
     {
-        
+        startWrite = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void addRecord(string ID, string objectName, float start, float end, string filePath) 
+    {
+        try
+        {
+            if (!startWrite)
+            {
+                using (StreamWriter file = new StreamWriter(@filePath, true))
+                {
+                    file.WriteLine(ID + "," + objectName + "," + start + "," + end);
+                }
+                startWrite = true;
+            }
+            else
+            {
+                using (StreamWriter file = new StreamWriter(@filePath, false))
+                {
+                    file.WriteLine(ID + "," + objectName + "," + start + "," + end);
+                }
+            }
+        } 
+        catch (Exception e)
+        {
+            throw new ApplicationException("Error: ", e);
+        }
+    }
+
+    private string GenerateFilePath()
+    {
+        return Application.dataPath + "/CSV/" + csvName + ".csv";
     }
 }
