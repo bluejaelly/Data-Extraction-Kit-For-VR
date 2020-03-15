326730f5bde2fba24235a0498100bcbe4a4b2c37
Jaelly <38776787+bluejaelly@users.noreply.github.com>
2020-03-15 00:56:34 -0500

    Improved Keyboard

---
+++ Assets/DataKit/Scripts/Key.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+using TMPro;
+
+public class Key : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI inputTextField;
+
+    private void Start()
+    {
+        inputTextField.text = "";
+    }
+
+    public void onClickKey()
+    {
+        inputTextField.text += gameObject.name;
+    }
+
+    public void onClickDelete()
+    {
+        try
+        {
+            inputTextField.text = inputTextField.text.Substring(0, inputTextField.text.Length - 1);
+        } 
+        catch (Exception e)
+        {
+            print("String length is 0");
+            inputTextField.text = "";
+        }
+    }
+
+    public void onClickClear()
+    {
+        inputTextField.text = "";
+    }
+}
