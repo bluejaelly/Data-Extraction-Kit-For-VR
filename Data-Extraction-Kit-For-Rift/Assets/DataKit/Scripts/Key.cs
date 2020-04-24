63e9269e25f56c10f641832aa350a745d6bee58d
Jaelly <38776787+bluejaelly@users.noreply.github.com>
2020-04-24 09:16:46 -0500

    Added Quest Build Files

---
+++ Data-Extraction-Kit-For-Rift/Assets/DataKit/Scripts/Key.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using UnityEngine;
+using TMPro;
+using VRTK;
+
+public class Key : MonoBehaviour
+{
+    [Header("Needs to be Assigned From Every Key!")]
+    [SerializeField] TextMeshProUGUI inputTextField;
+
+    [Header("Needs to be Assigned ONLY From the Done Key!")]
+    [SerializeField] VRTK_HeightAdjustTeleport heightAdjustTeleport;
+    [SerializeField] GameObject keyboardParent;
+    [SerializeField] GameObject dataCollectionPrefabsParent;
+
+
+    private void Start()
+    {
+        inputTextField.text = "";
+    }
+
+    public void onClickKey()
+    {
+        inputTextField.text += (gameObject.name).ToLower();
+    }
+
+    public void onClickDelete()
+    {
+        try
+        {
+            inputTextField.text = inputTextField.text.Substring(0, inputTextField.text.Length - 1);
+        } 
+        catch (Exception e)
+        {
+            print("String length is 0");
+            inputTextField.text = "";
+        }
+    }
+
+    public void onClickClear()
+    {
+        inputTextField.text = "";
+    }
+
+    public void onClickDone()
+    {
+        PlayerPrefs.SetString("ID", inputTextField.text);
+        inputTextField.text = "";
+        heightAdjustTeleport.GetComponent<VRTK_HeightAdjustTeleport>().enabled = true;
+        dataCollectionPrefabsParent.SetActive(true);
+        keyboardParent.SetActive(false);
+    }
+}
