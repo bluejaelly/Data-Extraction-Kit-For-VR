a2daa571ff680216116b71782253694eb4c109ad
Jihoon Kang <jihoon.kang@sibelhealth.com>
2020-09-28 00:37:58 -0500

    Dataset import update

--- Data_Visualization/app.py
+++ Data_Visualization/app.py
@@ -24,6 +24,61 @@ app = dash.Dash(
     meta_tags= [{"name": "viewport", "content": "width=device-width"}]
 )
 
+server = app.server
+
+# Import temp datasets
+head_data, point_data, grab_data, pos_data = importAllDatasets(os.getcwd() + '/*')
+
+####################
+# helper functions #
+####################
+
+
+def findIndex(ref_list, keyword):
+    try:
+        return [idx  for idx, s in enumerate(ref_list) if keyword in s][0]
+    except:
+        return None
+
+
+def importAllDatasets(directory):
+    """
+    A helper function to import all the datasets for the project from the input directory parameter.
+
+    Parameter
+    _________
+    directory : str
+        directory to the datasets
+
+    Returns
+    _______
+    head_data: Pandas Dataframe
+        A dataframe of a head gaze data
+
+    point_data: Pandas Dataframe
+        A dataframe of a controller point data
+
+    grab_data: Pandas Dataframe
+        A dataframe of a controller grab data
+
+    pos_data: Pandas Dataframe
+        A dataframe of a player position data
+    
+    """
+    head_index = findIndex(temp_list, "Gaze")
+    point_index = findIndex(temp_list, "Point")
+    grab_index = findIndex(temp_list, "Grab")
+    pos_index = findIndex(temp_list, "Position")
+
+    head_data = pd.read_csv(temp_list[head_index]) if head_index != None else None
+    point_data = pd.read_csv(temp_list[point_index]) if point_index != None else None
+    grab_data = pd.read_csv(temp_list[grab_index]) if grab_index != None else None
+    pos_data = pd.read_csv(temp_list[pos_index]) if pos_index != None else None
+
+
+    return head_data, point_data, grab_data, pos_data
+
+
 app.layout = html.Div(
     [
         dcc.Store(id="aggregate_data"),
@@ -156,7 +211,7 @@ app.layout = html.Div(
                                 )  
                             ]),
                             dcc.Tab(label='Player Position', value='position', className="custom-tab", style={"backgroundColor":"#e6e6e6"}, children=[
-                                dcc.Graph()
+                                dcc.Graph(id="position-heatmap", responsive=True, config={'displaylogo': False, 'responsive':True}, style={"height" : "100%", "width" : "100%"})
                             ]),
                         ]),
                         html.Div(id='cur-tab', style={'display':'none'})
@@ -170,10 +225,6 @@ app.layout = html.Div(
 )
 
 
-
-
-
-server = app.server
 
 if __name__ == "__main__":
     app.run_server(debug = True)
